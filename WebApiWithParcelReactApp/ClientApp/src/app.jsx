258f253dffe246e255d24aca5012bcaecfe931b8
RealWorldCode <real-world-code@outlook.com>
2023-02-26 21:25:16 -0500

    WebApiWithParcelReactApp - JavaScript

---
+++ WebApiWithParcelReactApp/ClientApp/src/app.jsx
@@ -0,0 +1,30 @@
+﻿import React, { useEffect, useState } from 'react'
+import './app.scss'
+
+export function App() {
+
+    const [forecast, setForecast] = useState('')
+
+    const [error, setError] = useState('')
+
+    useEffect(() => {
+        fetch("/api/WeatherForecast")
+            .then(r => r.json())
+            .then(data => setForecast(JSON.stringify(data, null, 4)))
+            .catch(err => setError(err.toString()))
+    }, [])
+
+    return <div>
+        <div>
+            <h1 className='hello'>Hello World!</h1>
+        </div>
+        {forecast && <div>
+            <div>Forecast:</div>
+            <pre>{forecast}</pre>
+        </div>}
+        {error && <div className=".error">
+            <div>Error:</div>
+            <pre>{error}</pre>
+        </div>}
+    </div>
+}
---
+++ WebApiWithParcelReactApp/ClientApp/src/index.js
@@ -0,0 +1,23 @@
+﻿import React from 'react'
+import { createRoot } from "react-dom/client";
+import { App } from './app';
+
+const container = document.getElementById("app")
+if (container) {
+    const root = createRoot(container)
+    root.render(<App />)
+}
+else {
+    const error = document.createElement("div")
+    error.innerText = "ERROR: No element found with id 'app'"
+    error.style.zIndex = "99999"
+    error.style.position = "fixed"
+    error.style.top = "0"
+    error.style.left = "0"
+    error.style.right = "0"
+    error.style.color = "red"
+    error.style.fontSize = "40px"
+    error.style.fontWeight = "bold"
+    error.style.textAlign = "center"
+    document.body.prepend(error);
+}
